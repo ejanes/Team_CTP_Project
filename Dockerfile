243cdb01c13268d42801546c274270537585f703
ejanes <eddiejanes2504@gmail.com>
2024-03-08 21:27:52 -0500

    initialize repo

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:20.11.1
+ENV HOME /root
+WORKDIR /root
+COPY . .
+EXPOSE 8080
+ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.2.1/wait /wait
+RUN chmod +x /wait
+CMD /wait && node app.js
---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080
+
+app.get('/',(req,res) => {
+    res.send('Welcome to our web app')
+})
+
+app.listen(port, () => {
+    console.log('app is listening on ' + port)
+})
