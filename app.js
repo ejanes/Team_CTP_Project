254e2f4e14df6863bc076885ac6c6f8df38e5327
rzhang25 <ruifengest@gmail.com>
2024-04-21 20:41:18 -0400

    part 2 o1 and o2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
 const app = express();
 const port = 8080;
@@ -9,9 +10,20 @@ const cookieParser = require('cookie-parser');
 const { getDB } = require('./db');
 const { ObjectId } = require('mongodb');
 const escape = require('escape-html');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/') 
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
 
 
+const upload = multer({ storage: storage });
 app.use(express.static('public'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -22,13 +34,44 @@ app.use((req, res, next) => {
 
 const { connectDB } = require('./db');
 connectDB().then(() => {
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`App listening on port ${port}`);
   });
 }).catch(err => {
   console.error('Failed to connect to MongoDB', err);
 });
 
+
+
+const WebSocket = require('ws');
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ noServer: true });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', async function incoming(message) {
+    const mes = JSON.parse(message);
+
+    if (mes.action === "placeBid" || mes.action === "placeCustomBid") {
+        const db = getDB();
+        const post = await db.collection('posts').findOne({ _id: ObjectId.createFromHexString(mes.postId) });
+
+        if (post && mes.newBid > post.currentBid) {
+            await db.collection('posts').updateOne(
+                { _id:  ObjectId.createFromHexString(mes.postId) },
+                { $set: { currentBid: mes.newBid, currentBidUser: mes.username } }
+            );
+
+            broadcastNewBid({ postId: mes.postId, newBid: mes.newBid, currentBidUser: mes.username });
+        }
+    }
+  });
+}); //ws work
+
+server.on('upgrade', function upgrade(request, socket, head) {
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, request);
+    });
+}); //ws upgrade
 
 app.post('/register', async (req, res) => {
     console.log(req.body);
@@ -126,20 +169,27 @@ app.get('/check-login', async (req, res) => {
 }); //check
 
 
-app.post('/posts', async (req, res) => {
+
+app.post('/posts', upload.single('media'), async (req, res) => {
     const db = getDB();
-    const { username } = req.body;
+    const { username, startingBid,} = req.body;
     let { content } = req.body; 
     content = escape(content);
+    const file = req.file;
 
-    const timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    const auctionDuration = parseInt(req.body.auctionDuration);
+    const endTime = new Date().getTime() + auctionDuration * 60 * 60 * 1000;
+
 
 
     const newPost = {
         username,
         content,
-        timestamp,
-        likes: [],
+        mediaPath: file ? file.path : null,
+        endTime,
+        startingBid: parseFloat(startingBid).toFixed(2),
+        currentBid: parseFloat(startingBid).toFixed(2),
+        currentBidUser: null,
     };
 
     await db.collection('posts').insertOne(newPost);
@@ -152,27 +202,39 @@ app.get('/posts', async (req, res) => {
     res.status(200).json(posts);
 }); //push the posts
 
-app.post('/posts/like', async (req, res) => {
-    const db = getDB();
-    const { postId, username } = req.body;
+// app.post('/posts/like', async (req, res) => {
+//     const db = getDB();
+//     const { postId, username } = req.body;
 
-    if (!username) {
-        return res.status(400).json({ message: 'user not found' });
-    }
+//     if (!username) {
+//         return res.status(400).json({ message: 'user not found' });
+//     }
 
-    const updated = await db.collection('posts').updateOne(
-        { _id: ObjectId.createFromHexString(postId) },
-        { $addToSet: { 'likes': username } }
-    );
+//     const updated = await db.collection('posts').updateOne(
+//         { _id: ObjectId.createFromHexString(postId) },
+//         { $addToSet: { 'likes': username } }
+//     );
 
-    if (updated.matchedCount === 0) {
-        return res.status(400).json({ message: 'post not found' });
-    }
+//     if (updated.matchedCount === 0) {
+//         return res.status(400).json({ message: 'post not found' });
+//     }
 
-    if (updated.modifiedCount === 0) {
-        return res.status(400).json({ message: 'You have already reacted to this post' });
-    }
+//     if (updated.modifiedCount === 0) {
+//         return res.status(400).json({ message: 'You have already reacted to this post' });
+//     }
 
-    res.status(200).json({ message: 'l success' });
-}); //like
+//     res.status(200).json({ message: 'l success' });
+// }); //like
 
+function broadcastNewBid(message) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({
+          type: 'updateBid',
+          postId: message.postId,
+          newBid: parseFloat(message.newBid).toFixed(2),
+          currentBidUser: message.currentBidUser
+        }));
+      }
+    });
+  }
--- public/functions.js
+++ public/functions.js
@@ -1,3 +1,6 @@
+let socket;
+
+
 function updateClock() {
     const now = new Date();
     const nowInEST = new Date(now.getTime() + (now.getTimezoneOffset() - 4 * 60) * 60000);
@@ -73,6 +76,7 @@ function handleLogin(event) {
         document.getElementById('loginUsername').value = '';
         document.getElementById('loginPassword').value = '';
         updateUI()
+        wsupgrade()
     })
     .catch(error => {
         console.error('L faild', error);
@@ -95,15 +99,7 @@ function handleLogout() {
             throw new Error('Lo faild');
         } else {
             localStorage.removeItem('username');
-
-            const loginSection = document.getElementById('loginForm');
-            const registerSection = document.getElementById('registerForm');
-            const userSection = document.getElementById('userSection');
-
-            loginSection.style.display = 'block';
-            registerSection.style.display = 'block';
-            userSection.style.display = 'none';
-            userSection.innerHTML = ''; 
+            updateUI();
         }
     })
     .catch(error => {
@@ -151,17 +147,27 @@ function updateUI() {
         const loginSection = document.getElementById('loginForm'); 
         const registerSection = document.getElementById('registerForm');
         const userSection = document.getElementById('userSection'); 
+        const postsSection = document.getElementById('postsSection');
+        const postForm = document.getElementById('postForm');
+        const loginPrompt = document.getElementById('loginPrompt');
 
         if (username) {
             loginSection.style.display = 'none'; 
             registerSection.style.display = 'none';
             userSection.style.display = 'block'; 
             userSection.innerHTML = `<p>Welcome, ${username}!</p><button onclick="handleLogout()">Log Out</button>`;
+            loginPrompt.style.display = 'none';
+            postsSection.style.display = 'block';
+            postForm.style.display = 'block';
+            wsupgrade();
         } else {
             loginSection.style.display = 'block'; 
             registerSection.style.display = 'block';
             userSection.style.display = 'none';
             userSection.innerHTML = '';
+            loginPrompt.style.display = 'block';
+            postsSection.style.display = 'none';
+            postForm.style.display = 'none';
         }
     });
 }
@@ -170,27 +176,42 @@ function updateUI() {
 function handlepost(event) {
     event.preventDefault();
     const content = document.getElementById('postContent').value;
+    const media = document.getElementById('postMedia').files[0]; 
+    const bid = document.getElementById('startingBid').value;
+    const duration = document.getElementById('auctionDuration').value;
+
+    const formData = new FormData();
+
+    formData.append('content', content);
+    formData.append('startingBid', bid);
+    formData.append('auctionDuration', duration);
+
+    if (media) {
+        formData.append('media', media);
+    }
 
     fetch('/check-login', { credentials: 'include' })
     .then(response => response.json())
     .then(data => {
         const username = data.loggedIn ? data.username : 'Guest';
-        go_to_post(content, username);
+        go_to_post(formData, username);
     })
     .catch(error => console.error('Error', error));
 }
 
-function go_to_post(content, username) {
+function go_to_post(formData, username) {
+    formData.append('username', username);
+
     fetch('/posts', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({username, content})
+        body: formData
     })
     .then(response => response.json())
     .then(data => {
         console.log('P success', data);
         fetchPosts(); 
         document.getElementById('postContent').value = ''; 
+        document.getElementById('postMedia').value = '';
     })
     .catch(error => {
         console.error('P failed', error);
@@ -206,10 +227,31 @@ function fetchPosts() {
         postsSection.innerHTML = ''; 
         posts.forEach(post => {
             const postElement = document.createElement('div');
-            postElement.innerHTML = `
-                <p>${post.username}: ${post.content} (Posted on: ${post.timestamp})</p>
-                <button onclick="handleLike('${post._id}')">Like (${post.likes.length})</button>
-            `;
+            const countdownTimer = calculateCountdown(post.endTime);
+
+            if (typeof post.currentBid === 'string') {
+                post.currentBid = parseFloat(post.currentBid);
+            }
+
+            postElement.innerHTML = `<p>${post.username}: ${post.content} (Time left: ${countdownTimer})</p>`;
+
+            if (post.mediaPath) {
+                if (post.mediaPath.includes('.mp4')) {
+                    postElement.innerHTML += `<video src="${post.mediaPath}" controls class="uploaded-media"></video>`;
+                } else if (post.mediaPath.includes('.mp3')) {
+                    postElement.innerHTML += `<audio src="${post.mediaPath}" controls class="uploaded-media"></audio>`;
+                } else {
+                    postElement.innerHTML += `<img src="${post.mediaPath}" alt="Posted Media" class="uploaded-media">`;
+                }
+            }
+            
+            postElement.innerHTML += `
+                <p>Current bid: $<span id="currentBid-${post._id}">${post.currentBid.toFixed(2)}</span></p>
+                <p>By: <span id="currentBidUser-${post._id}">${post.currentBidUser || 'Starting bid'}</span></p>
+                <button onclick="placeBid('${post._id}')">Bid ($<span id="bidIncrement-${post._id}">${minIncrease(post.currentBid, '1')}</span>)</button>
+                <input type="number" id="customBid-${post._id}" placeholder="Enter custom bid" min="${minIncrease(post.currentBid, '2')}">
+                <button onclick="placeCustomBid('${post._id}')">Custom Bid</button>
+                <p>(Websocket feature)</p>`;
             postsSection.appendChild(postElement);
         });
     })
@@ -219,26 +261,26 @@ function fetchPosts() {
 }
 
 
-function handleLike(postId) {
-    checkLogin().then(username => {
-        if (!username) {
-            console.log('not login');
-            return;
-        }
+// function handleLike(postId) {
+//     checkLogin().then(username => {
+//         if (!username) {
+//             console.log('not login');
+//             return;
+//         }
 
-        fetch('/posts/like', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ postId, username })
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.message);
-            fetchPosts(); 
-        })
-        .catch(error => console.error('Like failed', error));
-    });
-}
+//         fetch('/posts/like', {
+//             method: 'POST',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify({ postId, username })
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data.message);
+//             fetchPosts(); 
+//         })
+//         .catch(error => console.error('Like failed', error));
+//     });
+// }
 
 function initializePostHandlers() {
     const postForm = document.getElementById('postForm');
@@ -248,6 +290,98 @@ function initializePostHandlers() {
 }
 
 
+function wsupgrade() {
+    socket = new WebSocket('ws://localhost:8080/');
+  
+    socket.onopen = function(e) {
+      console.log("Connection established");
+    };
+  
+    socket.onmessage = function(event) {
+      console.log(`Data received from server: ${event.data}`);
+      try {
+        const message = JSON.parse(event.data);
+        
+        if (message.type === 'updateBid') {
+          const currentBidElement = document.getElementById(`currentBid-${message.postId}`);
+          const currentBidUserElement = document.getElementById(`currentBidUser-${message.postId}`);
+          const bidIncrementElement = document.getElementById(`bidIncrement-${message.postId}`);
+          const newBid = parseFloat(message.newBid);
+          
+          if (currentBidElement && currentBidUserElement && bidIncrementElement) {
+            currentBidElement.textContent = `${newBid.toFixed(2)}`;
+            currentBidUserElement.textContent = message.currentBidUser;
+            bidIncrementElement.textContent = minIncrease(parseFloat(message.newBid), '3');
+          }
+          
+          const customBidInput = document.getElementById(`customBid-${message.postId}`);
+          if (customBidInput) {
+            customBidInput.min = minIncrease(parseFloat(message.newBid), '4');
+          }
+        }
+      } catch (error) {
+        console.error('Error parsing message from server:', error);
+      }
+
+    };
+
+    socket.onerror = function(event) {
+        console.error('WebSocket error observed:', event);
+    };
+}
+
+function calculateCountdown(endTime) {
+    const now = new Date().getTime();
+    const timeLeft = endTime - now;
+    let hours = Math.floor(timeLeft / (1000 * 60 * 60));
+    let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  
+    return `${hours}h ${minutes}m`;
+  }
+
+
+function minIncrease(currentBid, checknum) {
+    Bid = parseFloat(currentBid);
+    if (isNaN(Bid)) {
+        console.error('Invalid currentBid:', currentBid, checknum);
+        return NaN;
+      }
+    return (Bid + Math.max(currentBid * 0.05, 1)).toFixed(2);
+}
+
+function placeBid(postId) {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        console.error('WebSocket is not connected.');
+        return;
+    }
+    const currentBidElement = document.getElementById(`currentBid-${postId}`);
+    const currentBid = parseFloat(currentBidElement.innerText);
+    const bidIncrement = minIncrease(currentBid, '5');
+    socket.send(JSON.stringify({
+        action: "placeBid",
+        postId: postId,
+        newBid: bidIncrement,
+        username: localStorage.getItem('username') 
+    }));
+}
+
+function placeCustomBid(postId) {
+    const customBidInput = document.getElementById(`customBid-${postId}`);
+    const customBid = parseFloat(customBidInput.value);
+    const minimumIncrement = minIncrease(parseFloat(document.getElementById(`currentBid-${postId}`).innerText), '6');
+
+    if(customBid < minimumIncrement) {
+        alert(`Your bid must be at least $${minimumIncrement}`);
+        return;
+    }
+    socket.send(JSON.stringify({
+        action: "placeCustomBid",
+        postId: postId,
+        newBid: customBid,
+        username: localStorage.getItem('username')
+    }));
+}
+
 
 
 document.addEventListener("DOMContentLoaded", updateUI);
@@ -256,3 +390,4 @@ document.addEventListener('DOMContentLoaded', () => {
     initializePostHandlers();
     fetchPosts(); 
 });
+
